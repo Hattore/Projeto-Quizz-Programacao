239f5ce33ceeb2e02d3b614a02ecc4437eec434f
unknown <andersonhattore@hotmail.com>
2022-07-27 16:26:38 -0300

    Projeto completo

---
+++ scripts2.js
@@ -0,0 +1,31 @@
+let teste ="olá";
+console.log(teste)
+
+
+const funcao1 = function teste1 (a,b) {
+    return a + b
+}
+
+const funcao2 = (a,b) => {
+    return a + b
+}
+
+console.log(funcao1(5,5));
+console.log(funcao2(5,3));
+
+
+//função
+
+//Para criar uma função
+
+let funcaoteste = function funcaodemo(a,b) {
+    return a * b
+}
+
+console.log(funcaoteste(10,10));
+
+
+
+//arrow function
+
+
